e8a77394ccd017bba14c782225ed019947de4566
Jaturaphum <suaprakhon2002gmail.com>
2024-07-19 18:14:29 +0700

    commit

--- LAB0303/php/edit_db.php
+++ LAB0303/php/edit_db.php
@@ -6,23 +6,18 @@ if (!isset($_GET['studentid']) || !isset($_SESSION['students'])) {
 
 $studentid = $_GET['studentid'];
 $student = null;
-
-foreach ($_SESSION['students'] as $s) {
-    if ($s['studentid'] == $studentid) {
-        $student = $s;
-        break;
-    }
-}
+$student = array_filter($students, fn($s) => $s['studentid'] === $studentid);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     foreach ($_SESSION['students'] as &$s) {
         if ($s['studentid'] == $studentid) {
-            $s['prefix'] = $_POST['prefix'];
-            $s['fname'] = $_POST['fname'];
-            $s['lname'] = $_POST['lname'];
-            $s['year'] = $_POST['year'];
-            $s['gpa'] = $_POST['gpa'];
-            $s['birthdate'] = $_POST['birthdate'];
+            // $s['prefix'] = $_POST['prefix'];
+            // $s['fname'] = $_POST['fname'];
+            // $s['lname'] = $_POST['lname'];
+            // $s['year'] = $_POST['year'];
+            // $s['gpa'] = $_POST['gpa'];
+            // $s['birthdate'] = $_POST['birthdate'];
+            $s = array_merge($s, $_POST);
             break;
         }
     }
